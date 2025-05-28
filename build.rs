6bb9fd9e5bbbedd89d01230656399709e94418fe
zeozeozeo <108888572+zeozeozeo@users.noreply.github.com>
2025-05-28 14:08:45 +0500

    +

--- build.rs
+++ build.rs
@@ -7,8 +7,9 @@ fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").expect("target_os not defined!");
     // on armv6 we need to link with libatomic
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("target_arch not defined!");
+    let target_env = env::var("CARGO_CFG_TARGET_ENV").expect("target_env not defined!"); // see Cargo.toml
 
-    if target_os == "linux" && target_arch == "arm" {
+    if target_os == "linux" && target_arch == "arm" && target_env != "musl" {
         // Embrace the atomic capability library across various platforms.
         // For instance, on certain platforms, llvm has relocated the atomic of the arm32 architecture to libclang_rt.builtins.a
         // while some use libatomic.a, and others use libatomic_ops.a.
